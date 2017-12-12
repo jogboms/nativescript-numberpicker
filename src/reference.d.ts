d9437ac10b3cce4ffa82d3b84f37828c21666bad
Jogboms <jeremiahogbomo@gmail.com>
2017-12-12 16:29:28 +0100

    chore: code clean up

--- src/reference.d.ts
+++ src/reference.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-
-declare const java: any;
-declare const com: any;
