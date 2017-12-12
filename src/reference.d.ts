3d1663c7d5749366d464f0649e710aeed00011b1
Jogboms <jeremiahogbomo@gmail.com>
2017-12-12 16:17:34 +0100

    chore: add native references

---
+++ src/reference.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+
+declare const java: any;
+declare const com: any;
